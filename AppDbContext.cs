722b0c0d293b3459f3ef2c8fb722abc4516ed442
Tu Nombre <siliezar970@gmail.com>
2024-11-17 22:29:21 -0600

    primer commit

---
+++ AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaTecnologia.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Producto> Productos { get; set; }
+}
